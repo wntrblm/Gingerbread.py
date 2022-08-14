4bc23fbe27187f8bd8c19c46684db0d8a3141c4a
Thea Flowers <thea@winterbloom.com>
2022-08-14 10:49:06 -0400

    Use KiCAD's bezier interpolation method

--- affinity2kicad/trace.py
+++ affinity2kicad/trace.py
@@ -15,12 +15,12 @@ import io
 import math
 import pathlib
 import sys
+from typing import Generator
 import uuid
 
 import gdstk
 import numpy as np
 import potracecffi
-import svgpathtools
 import pyvips
 from rich import print
 
@@ -29,28 +29,54 @@ def printe(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
-def bezier_to_points(seg, points=5):
-    num_lines = int(math.ceil(seg.length() / points))
-    pts = [seg.point(t) for t in np.linspace(0, 1, num_lines + 1)]
-    return pts
+# Ported from https://gitlab.com/kicad/code/kicad/-/blob/2ee65b2d83923acb71aa77ce0efab09a3f2a8f44/bitmap2component/bitmap2component.cpp#L544
+def bezier_to_points(p1, p2, p3, p4, delta=0.25):
+    # Approximate the curve by small line segments. The interval
+    # size, epsilon, is determined on the fly so that the distance
+    # between the true curve and its approximation does not exceed the
+    # desired accuracy delta.
+
+    # dd = maximal value of 2nd derivative over curve - this must occur at an endpoint.
+    dd0 = math.pow(p1[0] - 2 * p2[0] + p3[0], 2) + math.pow(
+        p1[1] - 2 * p2[1] + p3[1], 2
+    )
+    dd1 = math.pow(p2[0] - 2 * p3[0] + p4[0], 2) + math.pow(
+        p2[1] - 2 * p3[1] + p4[1], 2
+    )
+    dd = 6 * math.sqrt(max(dd0, dd1))
+    e2 = 8 * delta / dd if 8 * delta <= dd else 1
+    interval = math.sqrt(e2)
+
+    for t in np.arange(0, 1, interval):
+        x = (
+            p1[0] * math.pow(1 - t, 3)
+            + 3 * p2[0] * math.pow(1 - t, 2) * t
+            + 3 * p3[0] * (1 - t) * math.pow(t, 2)
+            + p4[0] * math.pow(t, 3)
+        )
+        y = (
+            p1[1] * math.pow(1 - t, 3)
+            + 3 * p2[1] * math.pow(1 - t, 2) * t
+            + 3 * p3[1] * (1 - t) * math.pow(t, 2)
+            + p4[1] * math.pow(t, 3)
+        )
+        yield (x, y)
+
+    yield p4
 
 
-def path_to_poly_pts(path):
-    pts = [potracecffi.curve_start_point(path.curve)]
+def path_to_poly_pts(path) -> Generator[tuple[float, float], None, None]:
+    last = potracecffi.curve_start_point(path.curve)
+    yield last
+
     for segment in potracecffi.iter_curve(path.curve):
         if segment.tag == potracecffi.CORNER:
-            pts.append(segment.c1)
-            pts.append(segment.c2)
+            yield segment.c1
+            yield segment.c2
         elif segment.tag == potracecffi.CURVETO:
-            b = svgpathtools.CubicBezier(
-                complex(*pts[-1]),
-                complex(*segment.c0),
-                complex(*segment.c1),
-                complex(*segment.c2),
-            )
-            for pt in bezier_to_points(b):
-                pts.append((pt.real, pt.imag))
-    return pts
+            yield from bezier_to_points(last, segment.c0, segment.c1, segment.c2)
+
+        last = segment.c2
 
 
 def load_image(path):
@@ -89,11 +115,11 @@ def trace_to_polys(bitmap: np.array) -> list:
         hole = path.sign == ord("-")
 
         if not hole:
-            p = gdstk.Polygon(pts)
+            p = gdstk.Polygon(list(pts))
             # printe(f"Poly {p.size} {p=}")
             polys.append(p)
         else:
-            hole = gdstk.Polygon(pts)
+            hole = gdstk.Polygon(list(pts))
             result = gdstk.boolean(polys.pop(), hole, "not")
             # printe(f"Hole {hole.size} {hole=} {result=}")
             polys.extend(result)
