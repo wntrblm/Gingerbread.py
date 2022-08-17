4f20b468e6563e79bed5a432534f2c353ba48930
Thea Flowers <thea@winterbloom.com>
2022-08-17 15:36:38 -0400

    Remove bezier_to_points from trace

--- affinity2kicad/trace.py
+++ affinity2kicad/trace.py
@@ -12,7 +12,6 @@
 
 import argparse
 import io
-import math
 import pathlib
 import sys
 from typing import Generator
@@ -25,42 +24,7 @@ import pyvips
 import rich
 
 from ._print import set_verbose, printv
-
-
-# Ported from https://gitlab.com/kicad/code/kicad/-/blob/2ee65b2d83923acb71aa77ce0efab09a3f2a8f44/bitmap2component/bitmap2component.cpp#L544
-def _bezier_to_points(p1, p2, p3, p4, delta=0.25):
-    # Approximate the curve by small line segments. The interval
-    # size, epsilon, is determined on the fly so that the distance
-    # between the true curve and its approximation does not exceed the
-    # desired accuracy delta.
-
-    # dd = maximal value of 2nd derivative over curve - this must occur at an endpoint.
-    dd0 = math.pow(p1[0] - 2 * p2[0] + p3[0], 2) + math.pow(
-        p1[1] - 2 * p2[1] + p3[1], 2
-    )
-    dd1 = math.pow(p2[0] - 2 * p3[0] + p4[0], 2) + math.pow(
-        p2[1] - 2 * p3[1] + p4[1], 2
-    )
-    dd = 6 * math.sqrt(max(dd0, dd1))
-    e2 = 8 * delta / dd if 8 * delta <= dd else 1
-    interval = math.sqrt(e2)
-
-    for t in np.arange(0, 1, interval):
-        x = (
-            p1[0] * math.pow(1 - t, 3)
-            + 3 * p2[0] * math.pow(1 - t, 2) * t
-            + 3 * p3[0] * (1 - t) * math.pow(t, 2)
-            + p4[0] * math.pow(t, 3)
-        )
-        y = (
-            p1[1] * math.pow(1 - t, 3)
-            + 3 * p2[1] * math.pow(1 - t, 2) * t
-            + 3 * p3[1] * (1 - t) * math.pow(t, 2)
-            + p4[1] * math.pow(t, 3)
-        )
-        yield (x, y)
-
-    yield p4
+from ._utils import bezier_to_points
 
 
 def _path_to_poly_pts(
@@ -74,7 +38,7 @@ def _path_to_poly_pts(
             yield segment.c1
             yield segment.c2
         elif segment.tag == potracecffi.CURVETO:
-            yield from _bezier_to_points(
+            yield from bezier_to_points(
                 last, segment.c0, segment.c1, segment.c2, delta=bezier_resolution
             )
 
@@ -147,7 +111,7 @@ def _trace_bitmap_to_polys(
 
 
 def _generate_fp_poly(
-    poly: list[gdstk.Polygon], *, layer: str, dpmm: float, output
+    poly: gdstk.Polygon, *, layer: str, dpmm: float, output
 ) -> str:
     output.write("  (fp_poly\n")
     output.write("    (pts \n")
