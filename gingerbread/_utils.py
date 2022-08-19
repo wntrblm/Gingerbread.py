9263cff0bbc5e02332265f138ad12dfa6951253f
Thea Flowers <thea@winterbloom.com>
2022-08-18 22:22:26 -0400

    Add _utils.path_to_points

--- gingerbread/_utils.py
+++ gingerbread/_utils.py
@@ -4,7 +4,10 @@
 
 import math
 import inspect
+from typing import Iterator
+
 import numpy as np
+import svgpathtools
 
 
 def default_param_value(function, name):
@@ -12,6 +15,24 @@ def default_param_value(function, name):
     params = sig.parameters
     return params[name].default
 
+
+def path_to_points(path: svgpathtools.Path, delta: float = 1) -> Iterator[tuple[float, float]]:
+    for seg in path:
+        if isinstance(seg, svgpathtools.Line):
+            yield (seg.start.real, seg.start.imag)
+            yield (seg.end.real, seg.end.imag)
+
+        elif isinstance(seg, svgpathtools.CubicBezier):
+            yield from bezier_to_points(
+                (seg.start.real, seg.start.imag),
+                (seg.control1.real, seg.control1.imag),
+                (seg.control2.real, seg.control2.imag),
+                (seg.end.real, seg.end.imag),
+                delta=delta
+            )
+
+        else:
+            raise ValueError(f"Can't convert path segment {seg=}.")
 
 # Ported from https://gitlab.com/kicad/code/kicad/-/blob/2ee65b2d83923acb71aa77ce0efab09a3f2a8f44/bitmap2component/bitmap2component.cpp#L544
 def bezier_to_points(p1, p2, p3, p4, delta=0.25):
