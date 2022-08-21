8d693fd54070a86a1dbb2082c707ad1f1026ebc6
Thea Flowers <thea@winterbloom.com>
2022-08-21 12:50:55 -0400

    Add _utils.compare_file_to_string

--- gingerbread/_utils.py
+++ gingerbread/_utils.py
@@ -4,9 +4,20 @@
 
 
 import inspect
+import pathlib
 
 
 def default_param_value(function, name):
     sig = inspect.signature(function)
     params = sig.parameters
     return params[name].default
+
+
+def compare_file_to_string(filename: pathlib.Path, contents: str) -> bool:
+    if not filename.exists():
+        return False
+
+    with open(filename, "r") as fh:
+        file_contents = fh.read()
+
+    return contents == file_contents
